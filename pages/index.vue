1e44145dc17a0f66ca3e417dc6fa39be3f33970b
Tom Quinlan <tquinlan1992@gmail.com>
2021-09-22 15:09:07 -0400

    use state

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <p>{{ dogFact }}</p>
+    <div v-if="state.dogFactOrSearch === 'RANDOM_DOG_FACT'">
+      <h4>Random Dog Fact</h4>
+      <p>{{ state.dogFact }}</p>
+    </div>
 
-    <button v-on:click="loadNewDogFact">Get New Dog Fact</button>
+    <div v-if="state.dogFactOrSearch === 'SEARCH'">
+      <h4>Search Results</h4>
+      <li v-for="dogFact2 in state.searchResults" :key="dogFact2">
+        {{ dogFact2 }}
+      </li>
+    </div>
 
-    <form v-on:submit="searchDogFacts">
-      <input v-model="search" placeholder="search dog fact" />
+    <button @click="loadNewDogFact">Get New Dog Fact</button>
+
+    <form @submit="searchDogFacts">
+      <input v-model="state.search" placeholder="search dog fact" />
     </form>
   </div>
 </template>
@@ -19,27 +29,47 @@ import {
   getAllDogFactsProxy,
 } from '../data/dog-facts'
 
+enum RandomDogFactOrSearch {
+  RANDOM_DOG_FACT = 'RANDOM_DOG_FACT',
+  SEARCH = 'SEARCH',
+}
+
 export default Vue.extend({
-  data(): { dogFact: string; search: string } {
+  data(): {
+    state: {
+      dogFact: string
+      search: string
+      searchResults: string[]
+      dogFactOrSearch: RandomDogFactOrSearch
+    }
+  } {
     return {
-      dogFact: '',
-      search: '',
+      state: {
+        dogFact: '',
+        search: '',
+        searchResults: [],
+        dogFactOrSearch: RandomDogFactOrSearch.RANDOM_DOG_FACT,
+      },
     }
   },
   async fetch() {
     const dogFacts = await getDogFacts()
-    this.dogFact = dogFacts.data[0].fact
+    this.state.dogFact = dogFacts.data[0].fact
   },
 
   methods: {
     async loadNewDogFact() {
       const dogFacts = await getDogFactsProxy()
-      this.dogFact = dogFacts.data[0].fact
+      this.state = {
+        ...this.state,
+        dogFactOrSearch: RandomDogFactOrSearch.RANDOM_DOG_FACT,
+        dogFact: dogFacts.data[0].fact,
+      }
     },
 
-    async searchDogFacts(event: any) {
+    async searchDogFacts(event: Event) {
       event.preventDefault()
-      const search = this.search
+      const search = this.state.search
       const allDogFacts = await getAllDogFactsProxy()
       const searchWords = search.split(' ')
       const result = allDogFacts.data.reduce<string[]>((result, dogFact) => {
@@ -56,7 +86,11 @@ export default Vue.extend({
           return result
         }
       }, [])
-      this.dogFact = result.join('Dog Fact: ')
+      this.state = {
+        ...this.state,
+        searchResults: result,
+        dogFactOrSearch: RandomDogFactOrSearch.SEARCH,
+      }
     },
   },
 })
